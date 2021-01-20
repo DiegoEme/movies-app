d5c226838722562308876710832ba9ce712110d5
Diego <soterg.uhdeia@gmail.com>
2021-01-20 00:15:09 -0500

    Layout and styles added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Movie from './components/Movie';
 import {useEffect, useState} from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const api = 'https://api.themoviedb.org/3/discover/movie?api_key=48967e7208298a4a6e2c66dbf510a727&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
 
@@ -23,9 +25,13 @@ useEffect(() => {
 }, [])
 
   return <div>
+    <Container  className="flex-wrap position-relative">
+    <Row  className="position-absolute">
     {movies.map((movie) => 
       <Movie key={movie.id} {...movie}/>
     )}
+    </Row>
+    </Container>
   </div>
 }
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Badge } from "react-bootstrap";
 
-const url_images = 'https://image.tmdb.org/t/p/w500/'
+const url_images = "https://image.tmdb.org/t/p/w500/";
 
-const Movie = ({title, poster_path, overview, vote}) => (
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+  
+    
+        
     <div className="movie">
-        <img src={url_images + poster_path} alt={title} />
+      <img src={url_images + poster_path} alt={title} />
+      <div className="movie-info">
+        <h6>{title}</h6>
+        <Badge variant="warning">{vote_average}</Badge> 
+      </div>
     </div>
+        
+    
+ 
 );
 
 export default Movie;
